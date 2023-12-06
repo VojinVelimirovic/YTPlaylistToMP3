0520cee6758b360768eae17fad703a812cd00f02
VojinVelimirovic <vojinvelimirovic2002@gmail.com>
2023-12-06 13:47:29 +0100

    version 1.2

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import pytube
 import os
 
-options = range(3)
+options = range(1, 3)
 counter = 0
 desktop_path = os.path.join(os.path.join(os.environ['USERPROFILE']), 'Desktop') + '\\'
 
@@ -31,53 +31,50 @@ def get_format():
 
 
 while True:
-    choice_made = False
-    choice = int(input("Main Menu:\n\n\t"
-                       "1. Download song\n\t"
-                       "2. Download playlist\n\n"
-                       "Enter a choice (1 - 2): "))
-    for option in options:
-        if choice == option:
-            choice_made = True
-    if choice_made:
-        break
+    choice = input("Options:\n\n\t"
+                   "1. Download video[mp3/mp4]\n\t"
+                   "2. Download playlist[mp3/mp4]\n\t"
+                   "X. Exit program\n\n"
+                   "Enter a choice (1 - 2 / X): ").lower()
 
-if choice == 1:
-    video = pytube.YouTube(input("Video link:"))
+    if choice == "1":
+        video = pytube.YouTube(input("Video link:"))
 
-    downloadFormat = get_format()
+        downloadFormat = get_format()
 
-    destination = desktop_path
-    print(f'Downloading: {video.title}...')
-    if downloadFormat == 1:
-        video.streams.filter(only_audio=True).first().download(output_path=destination)
-        base, ext = os.path.splitext(video)
-        new_file = base + '.mp3'
-        os.rename(video, new_file)
-    elif downloadFormat == 2:
-        video.streams.filter().first().download(output_path=destination)
-
-    print(video.title + " has successfully been downloaded onto your desktop.")
-
-
-elif choice == 2:
-    playlist = pytube.Playlist(input("Playlist link: "))
-
-    downloadFormat = get_format()
-
-    destination = desktop_path + playlist.title
-    make_folder()
-    out_files = []
-    print(f'Downloading: {playlist.title}...')
-    if downloadFormat == 1:
-        for video in playlist.videos:
-            out_files.append(video.streams.filter(only_audio=True).first().download(output_path=destination))
-        for out_file in out_files:
-            base, ext = os.path.splitext(out_file)
+        destination = desktop_path[0:-1]
+        print(destination)
+        print(f'Downloading: {video.title}...')
+        if downloadFormat == 1:
+            video.streams.filter(only_audio=True).first().download(output_path=destination)
+            base, ext = os.path.splitext(video)
             new_file = base + '.mp3'
-            os.rename(out_file, new_file)
-    elif downloadFormat == 2:
-        for video in playlist.videos:
-            out_files.append(video.streams.first().download(output_path=destination))
+            os.rename(video, new_file)
+        elif downloadFormat == 2:
+            video.streams.filter(resolution="720p").first().download(output_path=destination)
 
-    print(playlist.title + " has successfully been downloaded onto your desktop.")
+        print(video.title + " has successfully been downloaded onto your desktop.")
+
+    elif choice == "2":
+        playlist = pytube.Playlist(input("Playlist link: "))
+
+        downloadFormat = get_format()
+
+        destination = desktop_path + playlist.title
+        make_folder()
+        out_files = []
+        print(f'Downloading: {playlist.title}...')
+        if downloadFormat == 1:
+            for video in playlist.videos:
+                out_files.append(video.streams.filter(only_audio=True).first().download(output_path=destination))
+            for out_file in out_files:
+                base, ext = os.path.splitext(out_file)
+                new_file = base + '.mp3'
+                os.rename(out_file, new_file)
+        elif downloadFormat == 2:
+            for video in playlist.videos:
+                out_files.append(video.streams.filter(resolution="720p").first().download(output_path=destination))
+
+        print(playlist.title + " has successfully been downloaded onto your desktop.")
+    elif choice == "x":
+        break
